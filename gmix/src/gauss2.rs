26f968a0991e45e56fc35b38403dca72730ffe30
Erin Sheldon <erin.sheldon@gmail.com>
2015-09-20 14:29:27 -0400

    docs

--- gmix/src/gauss2.rs
+++ gmix/src/gauss2.rs
@@ -50,8 +50,11 @@ impl Gauss2 {
 
     pub fn eval(&self, pt: &Point2) -> f64 {
 
-        // we cannot evaluate until we have set the determinant
-        // and d* convenience fields
+        // we cannot evaluate until we have set the cov
+        // determinant and d* convenience fields, as well
+        // as the norm/pnorm fields. But we don't want
+        // this evaluation to return a Result or Option
+        
         assert!(self.norm_is_set);
 
         let xd = pt.x-self.cen.x;
